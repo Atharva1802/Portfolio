a93aeeb54ebe597269775a1ec93a46d8218cfbab
Atharva Bhawsar <atharvabhawsar18@gmail.com>
2025-03-20 19:39:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import AnimatedRoutes from "./components/AnimatedRoutes";
 function App() {
   const personalDetails = {
     name: "Atharva Bhawsar",
-    location: "Ujjain, India",
+    location: "Goa, India",
     email: "workmail.atharva@gmail.com",
     availability: "Open to work",
     brand:
-      "My brand is all about crafting impactful and seamless user interfaces that leave a lasting impression. As a Frontend Developer and content creator on Twitter and Hashnode, I specialize in HTML5, CSS, JavaScript, React, Vite, and VueJS. Let me help you bring your vision to life with my expertise in creating stunning and user-friendly interfaces.",
+      "A dynamic blend of designer, engineer, and data analyst, transforming ideas into impactful solutions. With expertise in Power BI, data visualization, and interactive dashboards, I turn complex data into clear, actionable insights. As a creative problem solver, I merge technical precision with design thinking to build intuitive experiences. Passionate about sustainability, technology, and innovation, I strive to deliver solutions that drive growth, efficiency, and meaningful change.",
   };
 
   return (
